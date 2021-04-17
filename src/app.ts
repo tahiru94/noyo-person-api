0c4319105227180d5d73078f26952a8cabc8ba28
tahiru94 <tahiru1994@engineer.com>
2021-04-16 22:27:45 -0400

    Integrate config mongoUrl with app
    * Setup mongoose / persistence connection (WIP)

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,30 @@
 import express, { Application } from 'express';
 import { json, urlencoded } from 'body-parser';
+import mongoose from 'mongoose';
 
 import Routes from './routes/person.route';
+import * as config from '../config';
 
 class App {
     public app: Application;
     public apiRoutes: Routes = new Routes();
+    public mongoUrl: string = config.default.mongoUrl;
 
     constructor() {
         this.app = express();
         this.config();
         this.apiRoutes.routes(this.app);
+        this.mongoSetup();
     }
 
     private config(): void {
         this.app.use(json());
         this.app.use(urlencoded({ extended: false }));
+    }
+
+    private mongoSetup(): void {
+        mongoose.Promise = global.Promise;
+        mongoose.connect(this.mongoUrl);
     }
 }
 
