5a576610ecedcb75859f7d1c96a5db12b025a49b
tahiru94 <tahiru1994@engineer.com>
2021-04-16 21:57:36 -0400

    Add the Person model / interface
    * Add mongoose dependency

---
+++ src/models/person.model.ts
@@ -0,0 +1,30 @@
+import { Schema } from 'mongoose';
+import { v4 } from 'uuid';
+
+const PersonSchema = new Schema({
+    id: {
+        type: Number,
+        default: v4() // auto-generated ID; different from MongoDB _id
+    },
+    firstName: {
+        type: String,
+        required: 'A first name must be entered for each Person.'
+    },
+    middleName: {
+        type: String
+    },
+    lastName: {
+        type: String,
+        required: 'A last name must be entered for each Person.'
+    },
+    email: {
+        type: String,
+        required: 'An email must be entered for each Person.'
+    },
+    age: {
+        type: Number,
+        required: 'An age must be entered for each Person.'
+    }
+});
+
+export default PersonSchema;
